bc529b753182a2f3bb4f0ffb98a1935aa72e8d36
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2023-03-29 11:48:16 +0200

    rm tty

--- singularity/test.sh
+++ singularity/test.sh
@@ -2,5 +2,5 @@
 
 docker run --privileged \
 	-v /etc/localtime:/etc/localtime \
-	--rm -it ins-amu/singularity \
+	--rm ins-amu/singularity \
 	singularity run --containall library://sylabsed/examples/lolcow
